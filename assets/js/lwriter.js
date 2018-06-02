bbbf5828619a673848d0704ed5928a5c9cdad2c5
Yusuf Canlı <cdario.done@gmail.com>
2018-06-02 03:11:53 +0300

    Update lwriter.js

--- assets/js/lwriter.js
+++ assets/js/lwriter.js
@@ -1,39 +1,87 @@
 $count = $('.lwriter').length;
+$('.lwriter').each(function () {
+    $(this).hide();
+})
 $text = $('.lwriter:eq(0)').attr('writer');
 $input = $('.lwriter:eq(0)');
 if ($input.get(0).hasAttribute('lwalign')) {
     $align = $input.attr('lwalign');
-    $input.css('text-align', $align);
+    if ($align == 'left') {
+        $input.css({
+            direction: 'lft',
+            textAlign: 'left'
+        });
+    }
+    if ($align == 'center') {
+        $input.css({
+            textAlign: 'center',
+            direction: 'unset'
+        })
+    }
+    if ($align == 'right') {
+        $input.css({
+            direction: 'rtl',
+            textAlign: $align
+        });
+    }
+}
+if ($input.get(0).tagName.toLowerCase() != 'input') {
+    $input.html('<span class="lwcarpet"></span>');
+    $('.lwcarpet').css({
+        borderRight: '1px solid'
+    })
 }
 $input.focus();
-$input.css('width', '' + $text.length + 'ch');
 $op = 1;
 $i = 0;
+$input.show();
 
-function lwriter() {
+function start() {
     if ($i != $text.length) {
         $speed = $input.attr('lwspeed');
-        $input.val($input.val() + $text.charAt($i));
+        if ($input.get(0).tagName.toLowerCase() != 'input') {
+            $('.lwcarpet').before($text.charAt($i));
+        } else {
+            $input.val($input.val() + $text.charAt($i));
+            console.log($input.get(0).tagName.toLowerCase())
+        }
         $i++;
         setTimeout(function () {
-            lwriter();
+            start();
         }, $speed);
     } else {
-        if ($op >= $count) {
-
-        } else {
+        $input.blur();
+        $('.lwcarpet').remove();
+        if ($op >= $count) {} else {
             $text = $('.lwriter:eq(' + $op + ')').attr('writer');
             $input = $('.lwriter:eq(' + $op + ')');
-            $input.css('width', '' + $text.length + 'ch');
             if ($input.get(0).hasAttribute('lwalign')) {
                 $align = $input.attr('lwalign');
-                $input.css('text-align', $align);
+                if ($align != 'right') {
+                    $input.css({
+                        direction: 'lft',
+                        textAlign: 'left'
+                    });
+                }
+                if ($align == 'right') {
+                    $input.css({
+                        direction: 'rtl',
+                        textAlign: $align
+                    });
+                }
             }
-            $input.focus();
+            if ($input.get(0).tagName.toLowerCase() != 'input') {
+                $input.html('<span class="lwcarpet"></span>');
+                $('.lwcarpet').css({
+                    borderRight: '1px solid'
+                })
+            }
             $op++;
             $i = 0;
+            $input.show();
+            $input.focus();
             setTimeout(function () {
-                lwriter();
+                start();
             }, 1000);
         }
     }
